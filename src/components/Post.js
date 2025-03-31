19ba1a5de4e3012444a288ecbadbaad5f419872d
AmanYadav8957 <152906478+AmanYadav8957@users.noreply.github.com>
2025-03-31 21:41:17 +0530

    Update Post.js

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,1 +1,13 @@
 
+import React from "react";  
+
+const Post = ({ author, content }) => {  
+    return (  
+        <div className="post">  
+            <h3>{author}</h3>  
+            <p>{content}</p>  
+        </div>  
+    );  
+};  
+
+export default Post;  
